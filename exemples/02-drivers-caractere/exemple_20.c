cf043a5e882675600dfe1ef71c11ff8e3d77b1da
Christophe Blaess <christophe.blaess@gmail.com>
2014-09-22 12:22:33 +0200

    [exemple_20] first version.

---
+++ exemples/02-drivers-caractere/exemple_20.c
@@ -0,0 +1,59 @@
+
+/************************************************************************\
+  exemple_20 - Chapitre - "Ecriture de driver - peripherique caractere"
+
+  Examen des adresses virtuelles, physiques, bus, et numero de page.
+
+  Exemples de la formation "Programmation Noyau sous Linux"
+
+  (c) 2005-2014 Christophe Blaess
+  http://www.blaess.fr/christophe/
+
+\************************************************************************/
+
+#include <linux/fs.h>
+#include <linux/mm.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+
+#include <asm/io.h>
+
+
+static int __init exemple_init (void)
+{
+	char * buffer;
+	struct page * pg;
+	unsigned int pfn;
+
+	buffer = kmalloc(256, GFP_KERNEL);
+	if (buffer == NULL)
+		return -ENOMEM;
+	
+	printk("%s: kmalloc() -> %p\n",
+	       THIS_MODULE->name, buffer);
+	printk("%s: virt-to-phys() -> %llu\n",
+	       THIS_MODULE->name, virt_to_phys(buffer));
+	printk("%s: virt-to-bus() -> %llu\n",
+	       THIS_MODULE->name, virt_to_bus(buffer));
+
+	pfn = virt_to_phys(buffer) >> PAGE_SHIFT;
+	printk("%s: pfn -> %x\n",
+	       THIS_MODULE->name, pfn);
+
+	pg = pfn_to_page(pfn);
+	if (pg != NULL)
+		printk("%s: page_address -> %p\n",
+		       THIS_MODULE->name, page_address(pg));
+	kfree(buffer);
+	return 0; 
+}
+
+
+static void __exit exemple_exit (void)
+{
+}
+
+module_init(exemple_init);
+module_exit(exemple_exit);
+MODULE_LICENSE("GPL");
+
