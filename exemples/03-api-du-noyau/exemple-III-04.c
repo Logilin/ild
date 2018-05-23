0e90ef1f862008c3d7210fe2f13507fd2057c10e
Christophe Blaess <christophe.blaess@logilin.fr>
2018-05-23 12:32:11 +0200

    Hack for 4.15 timer_setup modificatoin.

--- exemples/03-api-du-noyau/exemple-III-04.c
+++ exemples/03-api-du-noyau/exemple-III-04.c
@@ -10,35 +10,13 @@
 
 	#include <linux/module.h>
 	#include <linux/sched.h>
+	#include <linux/timer.h>
 	#include <linux/version.h>
 
-
-	static void exemple_timer_function(unsigned long);
 	static struct timer_list exemple_timer;
 
 
-static int __init exemple_init (void)
-{
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4,15,0)
-	init_timer (& exemple_timer);
-	exemple_timer.function = exemple_timer_function;
-	exemple_timer.data = (unsigned long) (& exemple_timer);
-#else
-	timer_setup (& exemple_timer, exemple_timer_function, (unsigned long) (& exemple_timer));
-#endif
-	exemple_timer.expires = jiffies + HZ;
-	add_timer(& exemple_timer);
-
-	return 0;
-}
-
-
-static void __exit exemple_exit (void)
-{
-	del_timer(& exemple_timer);
-}
-
-
 static void exemple_timer_function(unsigned long arg)
 {
 	struct timer_list * timer = (struct timer_list *) arg;
@@ -50,6 +28,42 @@ static void exemple_timer_function(unsigned long arg)
 	       time_of_day.tv_sec, time_of_day.tv_usec);
 
 	mod_timer(timer, jiffies + HZ);
+}
+#else
+static void exemple_timer_function(struct timer_list *timer);
+{
+	struct timeval time_of_day;
+
+	do_gettimeofday(& time_of_day);
+	printk(KERN_INFO "%s - %s: time_of_day=%ld.%06ld\n",
+	       THIS_MODULE->name, __FUNCTION__, 
+	       time_of_day.tv_sec, time_of_day.tv_usec);
+
+	mod_timer(timer, jiffies + HZ);
+}
+#endif
+
+
+
+static int __init exemple_init (void)
+{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,15,0)
+	init_timer (& exemple_timer);
+	exemple_timer.function = exemple_timer_function;
+	exemple_timer.data = (unsigned long) (& exemple_timer);
+#else
+	timer_setup (& exemple_timer, exemple_timer_function, 0);
+#endif
+	exemple_timer.expires = jiffies + HZ;
+	add_timer(& exemple_timer);
+
+	return 0;
+}
+
+
+static void __exit exemple_exit (void)
+{
+	del_timer(& exemple_timer);
 }
 
 
