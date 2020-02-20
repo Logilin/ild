8cdc5166ef8d571725339d69de65dad802b7184e
Christophe Blaess <christophe.blaess@logilin.fr>
2020-02-20 09:56:19 +0100

    [ADD] use module_misc_device() initialisation in exemple-IV-08.

--- exemples/04-driver-caracteres/exemple-IV-08.c
+++ exemples/04-driver-caracteres/exemple-IV-08.c
@@ -39,19 +39,6 @@
 	static volatile int current_pid = 0;
 	DEFINE_MUTEX(mtx_current_pid);
 
-
-static int __init example_init (void)
-{
-	return misc_register(& example_misc_driver);
-}
-
-
-static void __exit example_exit (void)
-{
-	misc_deregister(& example_misc_driver);
-}
-
-
 static ssize_t example_read(struct file * filp, char * buffer,
                             size_t length, loff_t * offset)
 {
@@ -83,8 +70,7 @@ static ssize_t example_read(struct file * filp, char * buffer,
 }
 
 
-	module_init(example_init);
-	module_exit(example_exit);
+	module_misc_device(example_misc_driver);
 
 	MODULE_DESCRIPTION("Use of a mutex for shared variable protection.");
 	MODULE_AUTHOR("Christophe Blaess <Christophe.Blaess@Logilin.fr>");
