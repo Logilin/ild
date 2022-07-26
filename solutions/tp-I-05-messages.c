1cd63e35e727a70c62452bd3f63d9ea4f92f98b0
Christophe Blaess <christophe.blaess@logilin.fr>
2022-07-26 11:53:13 +0200

    [ILD] Add solution for I-05 exercise.

---
+++ solutions/tp-I-05-messages.c
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-2.0
+//
+// Exemples de la formation
+//  "Ecriture de drivers et programmation noyau Linux"
+// Chapitre "Le noyau Linux et ses modules"
+//
+// (c) 2001-2022 Christophe Blaess
+//
+//    https://www.logilin.fr/
+//
+
+
+#include <linux/module.h>
+
+
+int __init example_00_init(void)
+{
+	/* Module initialization. */
+	printk(KERN_INFO "Hello from my module\n");
+	return 0;
+}
+
+
+void __exit example_00_exit(void)
+{
+	/* Module cleanup. */
+	printk(KERN_INFO "Bye bye!\n");
+}
+
+
+module_init(example_00_init);
+module_exit(example_00_exit);
+
+
+MODULE_DESCRIPTION("Empty module skeleton");
+MODULE_AUTHOR("My Name");
+MODULE_LICENSE("GPL v2");
+
