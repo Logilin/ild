b39866f6e85440d0441330c4ca1cf9ec3cf588e3
Christophe Blaess <christophe.blaess@logilin.fr>
2022-07-26 14:43:55 +0200

    [ILD] Add solution for II-01 exercise.

---
+++ solutions/tp-II-01-hello-world.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0
+//
+// Exemples de la formation
+//  "Ecriture de drivers et programmation noyau Linux"
+//
+// (c) 2001-2022 Christophe Blaess
+//
+//    https://www.logilin.fr/
+//
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void)
+{
+	char hostname[HOST_NAME_MAX + 1];
+
+	gethostname(hostname, HOST_NAME_MAX);
+
+	printf("Hello from %s\n", hostname);
+
+	return EXIT_SUCCESS;
+}
