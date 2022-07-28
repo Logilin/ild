869fc22ed40e4cf8a8f3e55a055e7617e4af9076
Christophe Blaess <christophe.blaess@logilin.fr>
2022-07-28 08:37:10 +0200

    [ADD] solution for exercise III-01.

---
+++ solutions/tp-III-01-timing.c
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: GPL-2.0
+//
+// Exemples de la formation
+//  "Ecriture de drivers et programmation noyau Linux"
+//
+// (c) 2001-2022 Christophe Blaess
+//
+//    https://www.logilin.fr/
+//
+
+#include <linux/jiffies.h>
+#include <linux/module.h>
+#include <linux/version.h>
+
+#define NB_SAMPLES    50
+
+static int __init timing_init(void)
+{
+	int i;
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 17, 0)
+	ktime_t  sample[NB_SAMPLES];
+
+	for (i = 0; i < NB_SAMPLES; i++)
+		sample[i] = ktime_get_real_ns();
+	for (i = 0; i < NB_SAMPLES; i++)
+		pr_info("%02d: %lld\n", i, sample[i]);
+
+#else
+	struct timespec sample[NB_SAMPLES];
+	for (i = 0; i < NB_SAMPLES; i++)
+		getnstimeofday(&(sample[i]));
+	for (i = 0; i < NB_SAMPLES; i++)
+		pr_info("%02d: %ld%09ld\n", i, sample[i].tv_sec, sample[i].tv_nsec);
+#endif
+
+	return 0;
+}
+
+
+static void __exit timing_exit(void)
+{
+}
+
+
+module_init(timing_init);
+module_exit(timing_exit);
+
+MODULE_DESCRIPTION("Exercise III-01 solution.");
+MODULE_AUTHOR("Christophe Blaess <Christophe.Blaess@Logilin.fr>");
+MODULE_LICENSE("GPL v2");
